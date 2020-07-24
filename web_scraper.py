f37d3fd279b4a337fd90355bbf3819442b9b1c13
Richard Whitehead <damian253@me.com>
2020-07-23 20:08:08 -0700

    lab almost complete

--- web_scraper.py
+++ web_scraper.py
@@ -3,6 +3,13 @@ from bs4 import BeautifulSoup
 
 URL = 'https://en.wikipedia.org/wiki/Mediterranean_Sea'
 
+def get_soup(URL):
+    response = requests.get(URL)
+    content = response.content
+    soup = BeautifulSoup(content, 'html.parser')
+    
+    return soup 
+
 response = requests.get(URL)
 
 content = response.content
@@ -11,7 +18,7 @@ soup = BeautifulSoup(content, 'html.parser')
 
 result = soup.find_all('a', title= 'Wikipedia:Citation needed')
 
-print (len (result))
+print (len(result))
 
 for citation in result:
     print (citation.parent.parent.parent.text)
